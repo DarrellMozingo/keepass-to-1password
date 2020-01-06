62d63fe598d7e680708fc7edb8b2b3cc7b48afa8
Darrell Mozingo <darrell@mozingo.net>
2020-01-06 13:31:03 -0500

    Write out csv conversion

--- convert.py
+++ convert.py
@@ -1,24 +1,37 @@
 from lxml import etree
+import csv
 
 def main():
     tree = etree.parse("Passwords.xml")
+
+    csv_rows = []
 
     for entry in tree.getroot().findall('.//Entry'):
         if entry.getparent().tag == 'History':
             continue
 
-        # title,website,username,password,notes
         print('title: ' + getField('Title', entry))
-        print('website: ' + getField('URL', entry))
-        print('username: ' + getField('UserName', entry))
-        print('password: ' + getField('Password', entry))
-        print('notes: ' + repr(getField('Notes', entry)))
+        print('website: {}'.format(getField('URL', entry)))
+        print('username: {}'.format(getField('UserName', entry)))
+        print('password: {}'.format(getField('Password', entry)))
+        print('notes: {}'.format(repr(getField('Notes', entry))))
         print('\n')
+
+        # title,website,username,password,notes
+        csv_rows += [[getField('Title', entry),
+                      getField('URL', entry),
+                      getField('UserName', entry),
+                      getField('Password', entry),
+                      getField('Notes', entry)]]
+
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(csv_rows)
 
 def getField(fieldName, entry):
     all_strings = entry.findall('./String')
     matching_string = next(filter(lambda e: e.find('Key').text == fieldName, all_strings))
-    return matching_string.find('./Value').text or '<empty>'
+    return matching_string.find('./Value').text
 
 if __name__ == "__main__":
     main()
